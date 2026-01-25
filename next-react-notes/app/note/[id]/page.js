125f36be0e4ed18284d2f38c48980a888a4821a9
ltlose <695236936@qq.com>
2026-01-25 23:35:14 +0800

    feat:notes开发

---
+++ next-react-notes/app/note/[id]/page.js
@@ -0,0 +1,26 @@
+import Note from "@/components/Note";
+import { getNote } from "@/lib/redis";
+
+export default async function Page({ params }) {
+  // 动态路由 获取笔记 id
+  // const noteId = params.id;
+  // 在 Next.js 15+ 中， params （以及 searchParams ）已经变成了 Promise ，而不再是直接的对象，所以需要await
+  const { id: noteId } = await params;
+  const note = await getNote(noteId);
+
+  // 为了让 Suspense 的效果更明显
+  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+  await sleep(2000);
+
+  if (note == null) {
+    return (
+      <div className="note--empty-state">
+        <span className="note-text--empty-state">
+          Click a note on the left to view something! 🥺
+        </span>
+      </div>
+    );
+  }
+
+  return <Note noteId={noteId} note={note} />;
+}
